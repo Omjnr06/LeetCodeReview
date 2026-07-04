ab499a57f4455c69881a01dba53abc59302f561c
Oladele Magbadelo Jr <omagbad@uwo.ca>
2026-07-04 17:32:11 +0300

    fix: changed words in daily temps from array to temperatures for easier understanding

--- Stacks/DailyTemperatures.py
+++ Stacks/DailyTemperatures.py
@@ -2,13 +2,13 @@
 # return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a 
 # warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
 
-def DailyTemperatures(array):
+def DailyTemperatures(temperatures):
 
-    result = [0] * len(array)
+    result = [0] * len(temperatures)
     stack = []
 
-    for x in range(len(array)):
-        while stack and array[x] > array[stack[-1]]:
+    for x in range(len(temperatures)):
+        while stack and temperatures[x] > temperatures[stack[-1]]:
             currIndex = stack.pop()
             result[x] = x - currIndex
         stack.append(currIndex)
